32e778a562daf82c8664bb365dfb2f01f8753119
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2025-03-27 14:50:37 -0700

    new VRT_VSC_Alloc() added

--- include/vrt.h
+++ include/vrt.h
@@ -59,6 +59,7 @@
  *
  * 22.0 (unpublished)
  *	VRT_VSC_Alloc() renamed to VRT_VSC_Allocv()
+ *	new VRT_VSC_Alloc() added
  * 21.0 (2025-03-17)
  *	VRT_u_req_grace() added
  *	VRT_u_req_ttl() added
@@ -826,6 +827,10 @@ void VRT_VSM_Cluster_Destroy(VRT_CTX, struct vsmw_cluster **);
 #ifdef va_start	// XXX: hackish
 void *VRT_VSC_Allocv(struct vsmw_cluster *, struct vsc_seg **,
     const char *, size_t, const unsigned char *, size_t, const char *, va_list);
+void *
+VRT_VSC_Alloc(struct vsmw_cluster *vc, struct vsc_seg **sg,
+    const char *nm, size_t sd,
+    const unsigned char *jp, size_t sj, const char *fmt, ...);
 #endif
 void VRT_VSC_Destroy(const char *, struct vsc_seg *);
 void VRT_VSC_Hide(const struct vsc_seg *);
